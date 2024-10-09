d48bf59809c4461c0979e669e54c2ef05b5250fe
captaingab <captaingab@Pilotgab-MacBook-Pro-2.local>
2024-10-09 17:29:45 +0100

    ci-update

--- apps/installation-tools.sh
+++ apps/installation-tools.sh
@@ -7,18 +7,18 @@ kubectl apply -n ingress-nginx -f html-site-ingress.yaml
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-kubectl apply -n argocd -f ../argocd-applications/deel-test-application.yaml
+kubectl apply -n argocd -f ../argocd-app/deel-test-application.yaml
 
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 kubectl create ns monitoring
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f prom-values.yaml -n monitoring
+helm upgrade --install prom prometheus-community/kube-prometheus-stack -f prom-values.yaml -n monitoring
 
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm upgrade --install promtail grafana/promtail -f promtail-values.yaml -n monitoring
 
-helm upgrade --install loki grafana/loki-distributed -n monitoring
+helm upgrade --install Loki grafana/loki-distributed -n monitoring
 
 
--- terra-infra/environments/deel-test-env/backend.tf
+++ terra-infra/environments/deel-test-env/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "deel-test"
-    key = "deel-test-1"
+    bucket = "deel-test-1"
+    key = "deel-test-infra"
     region = "eu-west-1" 
   }
 }
