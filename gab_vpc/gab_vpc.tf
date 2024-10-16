0b2e6ae239a9b2f838e33123682961cce4e11dfd
captaingab <captaingab@Pilotgab-MacBook-Pro-2.local>
2024-10-16 17:00:23 +0100

    gab_vpc

--- gab_vpc/gab_vpc.tf
+++ gab_vpc/gab_vpc.tf
@@ -16,68 +16,11 @@ module "gab_vpc" {
   enable_nat_gateway = true 
   single_nat_gateway = true  
 
-  enable_flow_log = true
-  flow_log_cloudwatch_iam_role_arn   = aws_iam_role.vpc_flow_log_role.arn
-
 
   tags = {
     Terraform = "true"
     Environment = "dev"
     Name = "gab_vpc"
-  }
-}
-
-
-resource "aws_iam_role" "vpc_flow_log_role" {
-  name = "vpcFlowLogRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "vpc-flow-logs.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "vpc_flow_log_policy" {
-  name   = "vpcFlowLogPolicy"
-  role   = aws_iam_role.vpc_flow_log_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
-
-resource "aws_cloudwatch_log_group" "vpc_flow_log_group" {
-  name              = "gab_vpc-flow-logs"
-  retention_in_days = 7 
-}
-
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = module.gab_vpc.vpc_id
-  service_name = "com.amazonaws.${var.region}.s3"
-
-  route_table_ids = module.gab_vpc.public_route_table_ids
-
-  tags = {
-    Name = "s3-vpc-endpoint"
   }
 }
 
