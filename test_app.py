1de33763394581b980693b19f0dcdf6064e0c018
captaingab <captaingab@Pilotgab-MacBook-Pro-2.local>
2024-10-27 21:30:55 +0100

    ci-update

--- test_app.py
+++ test_app.py
@@ -1,7 +1,6 @@
 import pytest
 from app import app, db, IP  # Removed 'os' import
 
-
 # Create a test client and configure the test environment
 @pytest.fixture
 def client():
@@ -13,7 +12,6 @@ def client():
         db.session.remove()
         db.drop_all()
 
-
 @pytest.fixture(autouse=True)
 def remove_ip_entries(client):
     # Clean up (remove entries) before each test
@@ -22,38 +20,39 @@ def remove_ip_entries(client):
         db.session.remove()
         db.drop_all()
 
-
 def test_display_ip(client):
     # Simulate a request to the display_ip route with X-Forwarded-For header
     response = client.get('/', headers={"X-Forwarded-For": "203.0.113.195"})
     assert response.status_code == 200
     assert b'Your IP is: <strong>203.0.113.195</strong>' in response.data
-    assert (b'Your reversed IP is: <strong>195.113.0.203</strong>'
-            in response.data)  # Trailing whitespace removed
+    assert (b'Your reversed IP is: <strong>195.113.0.203</strong>' in response.data)
 
     # Check if reversed IP is stored in the database
     with app.app_context():
         ip_entry = IP.query.filter_by(reversed_ip='195.113.0.203').first()
         assert ip_entry is not None
 
+def test_display_ip_without_x_forwarded_for(client):
+    # Simulate a request without X-Forwarded-For header
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Your IP is: <strong>127.0.0.1</strong>' in response.data
+    assert b'Your reversed IP is: <strong>1.0.0.127</strong>' in response.data
 
 def test_display_all(client):
     # Ensure display_all route works, initially should return an empty list
     response = client.get('/all')
     assert response.status_code == 200
-
     # Adding a sample IP entry
     with app.app_context():
         reversed_ip = '1.0.0.127'
         ip_entry = IP(reversed_ip=reversed_ip)
         db.session.add(ip_entry)
         db.session.commit()
-
     # Test again
     response = client.get('/all')
     assert response.status_code == 200
     assert reversed_ip.encode() in response.data
-
 
 def test_health_check(client):
     # Test the health check route
@@ -61,14 +60,12 @@ def test_health_check(client):
     assert response.status_code == 200
     assert b'Database connection successful' in response.data
 
-
 def test_create_database(client):
     # Test database creation
     with app.app_context():
         ip_entry = IP(reversed_ip='127.0.0.1')
         db.session.add(ip_entry)
         db.session.commit()
-
         assert IP.query.count() == 1
         # Test that the entry has been stored correctly
         retrieved_entry = IP.query.first()
