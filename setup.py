b2ad98cb72a7cba153cd8eaa5181c9a6e98ed1b4
captaingab <captaingab@Pilotgab-MacBook-Pro-2.local>
2024-10-29 17:02:00 +0100

    ci-update

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
-import os
 from setuptools import setup, find_packages
-
-
-def read_requirements(file):
-    """Read the requirements from a given file."""
-    with open(os.path.join(os.path.dirname(__file__), file)) as f:
-        return f.read().splitlines()
-
 
 setup(
     name='app',
     version='0.1',
     packages=find_packages(),
-    install_requires=read_requirements('requirements.txt'),
+    install_requires=[
+        'pytest-html',
+        'Flask-SQLAlchemy==3.1.1',
+        'Werkzeug==2.2.3',
+        'zipp>=3.19.1',
+        'Jinja2',
+        'prometheus_client',
+        'itsdangerous',
+        'kubernetes',
+        'pytest',
+        'pytest-mock',
+        'flask==2.2.5',
+        'MarkupSafe',
+        'click',
+        'build',
+        'psycopg2-binary',
+        'pytest-flask',
+        'pytest-cov',
+        'flake8',
+        'gunicorn==20.1.0',
+        'Flask-Testing',
+        'prometheus-flask-exporter==0.23.1',
+    ],
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'run-app=app:app',
+            'run-app=app:app'
         ],
     },
 )
