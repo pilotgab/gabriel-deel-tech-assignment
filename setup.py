1608ad2d030ab7725818677b07974cb28ae6c741
captaingab <captaingab@Pilotgab-MacBook-Pro-2.local>
2024-10-29 16:25:35 +0100

    ci-update

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
-# Read the dependencies from requirements.txt
+
 def read_requirements(file):
+    """Read the requirements from a given file."""
     with open(file) as f:
         return f.read().splitlines()
+
 
 setup(
     name='app',
     version='0.1',
-    packages=find_packages(),  # Automatically discover packages
-    install_requires=read_requirements('requirements.txt'),  # Read from requirements.txt
-    include_package_data=True,  # Include other files specified in MANIFEST.in
+    packages=find_packages(),
+    install_requires=read_requirements('requirements.txt'),
+    include_package_data=True,
     entry_points={
         'console_scripts': [
-            'run-app=app:app'
+            'run-app=app:app',
         ],
     },
 )
